6d1a44c61dbf41eaf81996bcc667fcb87bd06d40
Mutiarahmi Khairunnisa <87843671+mutiarahmikhairunnisa@users.noreply.github.com>
2022-04-08 13:34:21 +0700

    Update index.ts

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,8 +4,7 @@ import Todo from '../pages/todo/index.vue'
 import Profile from '../pages/profile/index.vue'
 
 const routes = [
-{name: Login,
-  path: '/login', 
+{ path: '/login', 
   component: Login},
 
 {path: '/todo',
