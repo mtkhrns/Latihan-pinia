0344977403a99dee0f05236932a775ef519b4304
Mutiarahmi Khairunnisa <87843671+mutiarahmikhairunnisa@users.noreply.github.com>
2022-04-08 13:52:20 +0700

    Update

--- vite.config.ts
+++ vite.config.ts
@@ -7,9 +7,7 @@ import Pages from 'vite-plugin-pages'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    vue(),
-    Pages(),
-    ],
+    vue()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
