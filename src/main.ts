ba13d89e9ca01d03286b728848a55f72db14131e
Mutiarahmi Khairunnisa <87843671+mutiarahmikhairunnisa@users.noreply.github.com>
2022-04-08 14:21:47 +0700

    Update

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,4 @@ import router from './router'
 const app = createApp(App)
 
 app.use(createPinia())
-app.use(router)
-
 app.mount('#app')
--- src/router/index.ts
+++ src/router/index.ts
@@ -3,7 +3,8 @@ import Login from '../pages/login/index.vue'
 import Todo from '../pages/todo/index.vue'
 import Profile from '../pages/profile/index.vue'
 
-const routes = [
+
+const router = [
 { path: '/login', component: Login},
 
 {path: '/todo', component: Todo},
@@ -11,4 +12,4 @@ const routes = [
 {path: '/profile',component: Profile }
 ]
 
-export default routes;  
+export default router;  
